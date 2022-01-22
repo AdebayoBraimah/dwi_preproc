344567d492f02e6d7425c1924c4a4344d5027c8c
AdebayoBraimah <adebayo.braimah@gmail.com>
2022-01-21 22:33:27 -0500

    RF: Added functionality to selectively submit jobs

--- wrapper.dwi.sh
+++ wrapper.dwi.sh
@@ -23,50 +23,69 @@ export PATH=${PATH}:~/bin/MRtrix/MRtrixSS3T/MRtrix3Tissue_linux/bin
 export PYTHONPATH=${PYTHONPATH}:$(which python3):$(which python2)
 
 scripts_dir=$(echo $(dirname $(realpath ${0})))
+struct_dir=/data/AICAD-HeLab/Data_TeamShare/dHCP_work/CINEPS/t2_work/struc_processed/derivatives_corrected
+rawdata=/data/AICAD-HeLab/tmp/tmp.eps/EPS/CINEPS/BIDS/rawdata
+derivatives=/data/AICAD-HeLab/tmp/tmp.eps/EPS/CINEPS/BIDS/derivatives
+
+
+# Template variables
+## AAL
+aal_template=$(realpath ../fmri_preproc/fmri_preproc_jobs/fmri_preproc/fmri_preproc/resources/atlases/UNC_infant_atlas_2020/atlas/templates/infant-neo-withSkull.nii.gz)
+aal_template_brain=$(realpath ../fmri_preproc/fmri_preproc_jobs/fmri_preproc/fmri_preproc/resources/atlases/UNC_infant_atlas_2020/atlas/templates/infant-neo-withCerebellum.nii.gz)
+aal_labels=$(realpath ../fmri_preproc/fmri_preproc_jobs/fmri_preproc/fmri_preproc/resources/atlases/UNC_infant_atlas_2020/atlas/templates/infant-neo-aal.nii.gz)
 
 shells=( 800 2000 )
 TEs=( 88 93 )
-sub=186
+subs=( $(cd ${rawdata}; ls -d sub-* | sed "s@sub-@@g" ) )
 
-output_dir=${scripts_dir}/test_data/test_proc
+# output_dir=${scripts_dir}/test_data/test_proc
 # rm -rf ${output_dir}
+output_dir=${derivatives}/dwi_preproc
 
 # Test code
-for (( i=0; i < ${#shells[@]}; i++ )); do
-  # Gather files
-  dwi=$(realpath /data/AICAD-HeLab/tmp/tmp.eps/EPS/CINEPS/BIDS/rawdata/sub-${sub}/dwi/sub-*_acq-b${shells[$i]}_dir-PA_run-01_dwi.nii.gz)
-  bval=$(remove_ext ${dwi}).bval
-  bvec=$(remove_ext ${dwi}).bvec
-  json=$(remove_ext ${dwi}).json
-  sbref=$(realpath /data/AICAD-HeLab/tmp/tmp.eps/EPS/CINEPS/BIDS/rawdata/sub-${sub}/dwi/sub-*_acq-*${TEs[$i]}_dir-*_run-01_sbref.nii.gz)
+for sub in ${subs[@]}; do
+  for (( i=0; i < ${#shells[@]}; i++ )); do
+    # Gather files
+    dwis=( $(realpath ${rawdata}/sub-${sub}/dwi/sub-*_acq-b${shells[$i]}_dir-PA_run-*_dwi.nii.gz) )
 
-  slspec=${scripts_dir}/misc/b${shells[$1]}/*.slice_order
-  acqp=${scripts_dir}/misc/b${shells[$1]}/*.acq*
+    for dwi in ${dwis[@]}; do
+      bval=$(remove_ext ${dwi}).bval
+      bvec=$(remove_ext ${dwi}).bvec
+      json=$(remove_ext ${dwi}).json
+      sbref=$(realpath ${rawdata}/sub-${sub}/dwi/sub-*_acq-*${TEs[$i]}_dir-*_run-01_sbref.nii.gz)
 
-  # Tractography test files
-  ## AAL
-  aal_template=$(realpath ../fmri_preproc/fmri_preproc_jobs/fmri_preproc/fmri_preproc/resources/atlases/UNC_infant_atlas_2020/atlas/templates/infant-neo-withSkull.nii.gz)
-  aal_template_brain=$(realpath ../fmri_preproc/fmri_preproc_jobs/fmri_preproc/fmri_preproc/resources/atlases/UNC_infant_atlas_2020/atlas/templates/infant-neo-withCerebellum.nii.gz)
-  aal_labels=$(realpath ../fmri_preproc/fmri_preproc_jobs/fmri_preproc/fmri_preproc/resources/atlases/UNC_infant_atlas_2020/atlas/templates/infant-neo-aal.nii.gz)
+      if [[ -f ${dwi} ]] && [[ -f ${bval} ]] && [[ -f ${bvec} ]] && [[ -f ${sbref} ]]; then
+        slspec=${scripts_dir}/misc/b${shells[$i]}/*.slice_order
+        acqp=${scripts_dir}/misc/b${shells[$i]}/*.acq*
 
-  ## dHCP (use single subject files)
-  struct_dir=/data/AICAD-HeLab/Data_TeamShare/dHCP_work/CINEPS/t2_work/struc_processed/derivatives_corrected
-  template=$(realpath ${struct_dir}/sub-${sub}/ses-*/anat/*T2w_restore.nii.*)
-  template_brain=$(realpath ${struct_dir}/sub-${sub}/ses-*/anat/*T2w_restore_brain.nii.*)
-  labels=$(realpath ${struct_dir}/sub-${sub}/ses-*/anat/*drawem_all_labels.nii.*)
+        # Tractography test files
+        ## dHCP (use single subject files)
+        template=$(realpath ${struct_dir}/sub-${sub}/ses-*/anat/*T2w_restore.nii.*)
+        template_brain=$(realpath ${struct_dir}/sub-${sub}/ses-*/anat/*T2w_restore_brain.nii.*)
+        labels=$(realpath ${struct_dir}/sub-${sub}/ses-*/anat/*drawem_all_labels.nii.*)
+        cmd=""
 
-  bsub -n 1 -R "span[hosts=1]" -q gpu-v100 -gpu "num=1" -M 20000 -W 8000 \
-  ${scripts_dir}/dwi_preproc.sh \
-  --dwi ${dwi} \
-  --bval ${bval} \
-  --bvec ${bvec} \
-  --sbref ${sbref} \
-  --dwi-json ${json} \
-  --slspec ${slspec} \
-  --acqp ${acqp} \
-  --data-dir ${output_dir} \
-  --template ${aal_template} --template-brain ${aal_template_brain} --labels ${aal_labels} --out-tract AAL \
-  --template ${template} --template-brain ${template_brain} --labels ${labels} --out-tract dHCP_40wk
+        if [[ -f ${template} ]] && [[ -f ${template_brain} ]] && [[ -f ${labels} ]]; then
+          cmd="--template ${template} --template-brain ${template_brain} --labels ${labels} --out-tract dHCP_40wk"
+        fi
+
+        # bsub -J ${sub}_${shells[$i]} -n 1 -R "span[hosts=1]" -M 20000 -W 8000 \
+        bsub -J ${sub}_${shells[$i]} -n 1 -R "span[hosts=1]" -q gpu-v100 -gpu "num=1" -M 20000 -W 8000 \
+        ${scripts_dir}/dwi_preproc.sh \
+        --dwi ${dwi} \
+        --bval ${bval} \
+        --bvec ${bvec} \
+        --sbref ${sbref} \
+        --dwi-json ${json} \
+        --slspec ${slspec} \
+        --acqp ${acqp} \
+        --data-dir ${output_dir} \
+        --template ${aal_template} --template-brain ${aal_template_brain} \
+        --labels ${aal_labels} --out-tract AAL ${cmd}
+        # echo "bsub -J ${sub}_${shells[$i]} -n 1 -R "span[hosts=1]" -q gpu-v100 -gpu "num=1" -M 20000 -W 8000 ${scripts_dir}/dwi_preproc.sh --dwi ${dwi} --bval ${bval} --bvec ${bvec} --sbref ${sbref} --dwi-json ${json} --slspec ${slspec} --acqp ${acqp} --data-dir ${output_dir} --template ${aal_template} --template-brain ${aal_template_brain} --labels ${aal_labels} --out-tract AAL ${cmd}" >> test.file.sh
+      fi
+    done
+  done
 done
 
 
