85a4e2f34d71cf693cf93b2ed95aef9aecbcf74d
AdebayoBraimah <adebayo.braimah@gmail.com>
2022-05-21 15:20:51 -0400

    MNT: Changed variable references

--- wrapper.dwi.sh
+++ wrapper.dwi.sh
@@ -23,10 +23,10 @@ export PATH=${PATH}:~/bin/MRtrix/MRtrixSS3T/MRtrix3Tissue_linux/bin
 export PYTHONPATH=${PYTHONPATH}:$(which python3):$(which python2)
 
 # scripts_dir=$(echo $(dirname $(realpath ${0})))
-scripts_dir=/data/AICAD-HeLab/tmp/tmp.eps/EPS/CINEPS/BIDS/code/dwi_preproc
+scripts_dir=/data/AICAD-HeLab/Data_TeamShare/dHCP_work/EPS.BIDS/EPS/CINEPS/BIDS/code/dwi_preproc
 struct_dir=/data/AICAD-HeLab/Data_TeamShare/dHCP_work/CINEPS/t2_work/struc_processed/derivatives_corrected
-rawdata=/data/AICAD-HeLab/tmp/tmp.eps/EPS/CINEPS/BIDS/rawdata
-derivatives=/data/AICAD-HeLab/tmp/tmp.eps/EPS/CINEPS/BIDS/derivatives
+rawdata=/data/AICAD-HeLab/Data_TeamShare/dHCP_work/EPS.BIDS/EPS/CINEPS/BIDS/rawdata
+derivatives=/data/AICAD-HeLab/Data_TeamShare/dHCP_work/EPS.BIDS/EPS/CINEPS/BIDS/derivatives
 
 
 # Template variables
@@ -35,11 +35,13 @@ aal_template=$(realpath ../fmri_preproc/fmri_preproc_jobs/fmri_preproc/fmri_prep
 aal_template_brain=$(realpath ../fmri_preproc/fmri_preproc_jobs/fmri_preproc/fmri_preproc/resources/atlases/UNC_infant_atlas_2020/atlas/templates/infant-neo-withCerebellum.nii.gz)
 aal_labels=$(realpath ../fmri_preproc/fmri_preproc_jobs/fmri_preproc/fmri_preproc/resources/atlases/UNC_infant_atlas_2020/atlas/templates/infant-neo-aal.nii.gz)
 
-mporder=8
+# mporder=8
+mporder=""
 
 shells=( 800 2000 )
 TEs=( 88 93 )
 subs=( $(cd ${rawdata}; ls -d sub-* | sed "s@sub-@@g" ) )
+factors=( 4 8 )
 
 # output_dir=${scripts_dir}/test_data/test_proc
 # rm -rf ${output_dir}
@@ -58,8 +60,8 @@ for sub in ${subs[@]}; do
       sbref=$(realpath ${rawdata}/sub-${sub}/dwi/sub-*_acq-*${TEs[$i]}_dir-*_run-01_sbref.nii.gz)
 
       if [[ -f ${dwi} ]] && [[ -f ${bval} ]] && [[ -f ${bvec} ]] && [[ -f ${sbref} ]]; then
-        slspec=${scripts_dir}/misc/b${shells[$i]}/*.slice_order
-        acqp=${scripts_dir}/misc/b${shells[$i]}/*.acq*
+        # slspec=${scripts_dir}/misc/b${shells[$i]}/*.slice_order
+        # acqp=${scripts_dir}/misc/b${shells[$i]}/*.acq*
 
         # Tractography test files
         ## dHCP (use single subject files)
@@ -72,20 +74,20 @@ for sub in ${subs[@]}; do
           cmd="--template ${template} --template-brain ${template_brain} --labels ${labels} --out-tract dHCP_40wk"
         fi
         
-        # bsub -J ${sub}_${shells[$i]} -n 1 -R "span[hosts=1]" -q gpu-v100 -gpu "num=1" -M 25000 -W 10000 \
-        bsub -J ${sub}_${shells[$i]} -n 1 -R "span[hosts=1]" -M 25000 -W 10000 \
+        # bsub -J ${sub}_${shells[$i]} -n 1 -R "span[hosts=1]" -M 25000 -W 10000 \
+        bsub -J ${sub}_${shells[$i]} -n 1 -R "span[hosts=1]" -q gpu-v100 -gpu "num=1" -M 25000 -W 10000 \
         ${scripts_dir}/dwi_preproc.sh \
         --dwi ${dwi} \
         --bval ${bval} \
         --bvec ${bvec} \
         --sbref ${sbref} \
         --dwi-json ${json} \
-        --slspec ${slspec} \
-        --acqp ${acqp} \
         --data-dir ${output_dir} \
         --mporder ${mporder} \
         --template ${aal_template} --template-brain ${aal_template_brain} \
         --labels ${aal_labels} --out-tract AAL ${cmd}
+        # --slspec ${slspec} \
+        # --acqp ${acqp} \
         # echo "bsub -J ${sub}_${shells[$i]} -n 1 -R "span[hosts=1]" -q gpu-v100 -gpu "num=1" -M 20000 -W 8000 ${scripts_dir}/dwi_preproc.sh --dwi ${dwi} --bval ${bval} --bvec ${bvec} --sbref ${sbref} --dwi-json ${json} --slspec ${slspec} --acqp ${acqp} --data-dir ${output_dir} --template ${aal_template} --template-brain ${aal_template_brain} --labels ${aal_labels} --out-tract AAL ${cmd}" >> test.file.sh
       fi
     done
